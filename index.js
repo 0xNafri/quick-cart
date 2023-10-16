72e7c0e067af545f0374a6050d07a81764655597
0xNafri <101189966+0xNafri@users.noreply.github.com>
2023-10-17 00:52:40 +0800

    added favicon + remove items feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 //Firebase credentials
 const appSettings = {
@@ -14,15 +14,36 @@ const inputEl = document.getElementById("input-el")
 const addEl = document.getElementById('btn-add')
 const listEl = document.getElementById("list-el")
 
+//Add items to database
 addEl.addEventListener("click", () => {
     let inputValue = inputEl.value
 
-    // push(cartDB, inputValue)
-    // console.log(`${inputValue} added to database`)
+    push(cartDB, inputValue)
+    console.log(`${inputValue} added to database`)
     // addListEl(inputValue)
     clearInputEl()
 })
 
+//Get data (items) from database
+onValue(cartDB, (snapshot) => {
+    
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        
+        clearListEl()
+        for(let i = 0; i < itemsArray.length; i++){
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            addListEl(currentItem)
+        }
+    } else {
+        listEl.innerHTML = "No items to display"
+    }
+})
+
+// Add + remove items to list element
 function addListEl(item) {
     // listEl.innerHTML += `<li>${itemValue}</li>`
     let itemID = item[0]
@@ -31,7 +52,14 @@ function addListEl(item) {
     let newEL = document.createElement("li")
     newEL.textContent = itemValue
     listEl.append(newEL)
+
+    newEL.addEventListener("dblclick", () =>{
+        let itemLocation = ref(database, `cart/${itemID}`)
+
+        remove(itemLocation)
+    })
 }
+
 
 function clearInputEl() {
     inputEl.value = ''
@@ -40,16 +68,3 @@ function clearInputEl() {
 function clearListEl() {
     listEl.innerHTML = ""
 }
-
-onValue(cartDB, (snapshot) => {
-    let itemsArray = Object.entries(snapshot.val())
-
-    clearListEl()
-    for(let i = 0; i < itemsArray.length; i++){
-        let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-
-        addListEl(currentItem)
-    }
-})
